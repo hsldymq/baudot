3c2e7753f78c07323a8ce4b704fdf69f84114ec3
hsldymq <hsldymq@qq.com>
2018-10-26 18:52:46 +0800

    update baudot definition

--- baudot.go
+++ baudot.go
@@ -7,24 +7,3 @@ const (
 	Figures  Charset = 1
 	Cyrillic Charset = 2
 )
-
-type Codec interface {
-	Encode(string) ([]byte, error)
-	Decode([]byte) (string, error)
-}
-
-func NewITA1() {
-
-}
-
-func NewITA2() {
-
-}
-
-func NewUSTTY() {
-
-}
-
-func NewMTK2() {
-
-}
