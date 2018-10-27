4bc3cc5aa59007252eb024aaa8e3f2fd452e62c9
hsldymq <hsldymq@qq.com>
2018-10-27 20:08:52 +0800

    更新文档注释

--- ita1.go
+++ ita1.go
@@ -5,24 +5,24 @@
 
 package baudot
 
-// Encode string into byte array represent the sequence of Baudot-Murray code(ITA2)
+// Encode string into byte array represent the sequence of Baudot code
 func (c *ita1) Encode(msg string) ([]byte, error) {
 	return encode(msg, c.ignErr, versionITA1)
 }
 
-// Decode Baudot-Murray code(ITA2) to string
+// Decode Baudot code to string
 func (c *ita1) Decode(codes []byte) (string, error) {
 	return decode(codes, c.ignErr, versionITA1)
 }
 
-// EncodeChar encodes a character into Baudot-Murray code(ITA2)
+// EncodeChar encodes a character into Baudot code
 func (c *ita1) EncodeChar(char rune, currentCharset Charset) (byte, bool, error) {
 	code, shiftedCharset, err := encodeChar(char, currentCharset, versionITA1)
 
 	return code, shiftedCharset != currentCharset, err
 }
 
-// DecodeChar decodes a Baudot-Murray code(ITA2) to rune
+// DecodeChar decodes a Baudot code to rune
 func (c *ita1) DecodeChar(code byte, currentCharset Charset) (rune, bool, error) {
 	char, shiftedCharset, err := decodeChar(code, currentCharset, versionITA1)
 
